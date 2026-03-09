adb8629ac070bdfdcba3814909a2d722d66952d7
JNU_AI_Ahn <profahn4027@gmail.com>
2026-03-09 21:58:10 +0900

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -7,11 +7,3 @@ def test_add():
 
 def test_subtract():
     assert subtract(5, 2) == 3
-
-
-def test_multiply():
-    assert multiply(3, 4) == 12
-
-
-def test_divide():
-	assert divide(10, 2) == 5
